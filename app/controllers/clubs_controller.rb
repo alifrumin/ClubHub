31023c8a3e9fc412b6dce476ad5d238adf95fbca
Alice Frumin <alifrumin@yahoo.com>
2015-11-16 16:30:37 -0500

    most meeting functionality working

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,8 @@
 class ClubsController < ApplicationController
+
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+	# before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   # index
   def index
     @clubs = Club.all
@@ -7,10 +11,12 @@ class ClubsController < ApplicationController
   # new
   def new
     @club = Club.new
+    @user = current_user
   end
 
   # create
   def create
+    @user = current_user
     @club = Club.new(club_params)
     if @club.save
       flash[:notice] = "#{@club.name} was successfully created."
@@ -23,11 +29,18 @@ class ClubsController < ApplicationController
   #show
   def show
     @club = Club.find(params[:id])
+    @meetings = @club.meetings
+
   end
 
   # edit
   def edit
     @club = Club.find(params[:id])
+    # @user = @club.user
+    # if @user != current_user
+    #   flash[:alert] = "Access denied! You can't edit someone else's pin."
+    #   redirect_to clubs_path
+    # end
   end
 
   # update
@@ -51,5 +64,9 @@ class ClubsController < ApplicationController
   private
   def club_params
     params.require(:club).permit(:name, :description, :img_url)
+  end
+
+  def set_post
+    @club = Club.find(params[:id])
   end
 end
---
+++ app/controllers/meetings_controller.rb
@@ -0,0 +1,61 @@
+class MeetingsController < ApplicationController
+
+	# before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+	def index
+		@meetings = Meeting.all
+	end
+
+	def new
+		@club = Club.find(params[:club_id])
+		# @user = @club.user
+		@meeting = Meeting.new
+		# if @user != current_user
+		# 	flash[:alert] = "Access denied! You can't add a meeting to someone else's club."
+		# 	redirect_to club_path(@club)
+		# end
+	end
+
+	def create
+		@club = Club.find(params[:club_id])
+		@meeting = Meeting.create!(meeting_params.merge(club: @club))
+		redirect_to club_path(@club)
+	end
+
+	def show
+		@club = Club.find(params[:club_id])
+		@meeting = Meeting.find(params[:id])
+		# @user = @club.user
+	end
+
+	def edit
+		@club = Club.find(params[:club_id])
+		@meeting = Meeting.find(params[:id])
+		# @user = @club.user
+		# if @user != current_user
+		# 	flash[:alert] = "Access denied! You can't edit someone else's pin."
+		# 	redirect_to club_meeting_path(@club, @meeting)
+		# end
+	end
+
+	def update
+		@club = Club.find(params[:club_id])
+		# @user = @club.user
+		@meeting = Meeting.find(params[:id])
+		@meeting.update(meeting_params.merge(club: @club))
+		redirect_to club_path(@club)
+	end
+
+	def destroy
+		@meeting = Meeting.find(params[:id])
+		@club = Board.find(params[:club_id])
+		@meeting.destroy
+		redirect_to club_path(@club)
+	end
+
+	private
+	def meeting_params
+		params.require(:meeting).permit(:club_id, :meeting_image, :attending, :invited, :host, :details, :theme, :user_id, :location, :datetime, :title)
+	end
+
+end
